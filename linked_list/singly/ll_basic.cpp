51694d5db13f35cd7121a4c970db3247093555c5
Raj Patil <rajvardhan.patil525@gmail.com>
2025-12-30 14:59:51 +0530

    Update ll_basic.cpp

--- linked_list/singly/ll_basic.cpp
+++ linked_list/singly/ll_basic.cpp
@@ -1,1 +1,29 @@
+#include <iostream>
+using namespace std;
+
+struct Node {
+    int data;
+    Node* next;
+    Node(int val) : data(val), next(nullptr) {}
+};
+
+void printList(Node* head) {
+    while(head != nullptr) {
+        cout << head->data << " -> ";
+        head = head->next;
+    }
+    cout << "nullptr\n";
+}
+
+int main() {
+    // Create nodes
+    Node* head = new Node(1);
+    head->next = new Node(2);
+    head->next->next = new Node(3);
+
+    cout << "Singly Linked List: ";
+    printList(head);
+
+    return 0;
+}
 
