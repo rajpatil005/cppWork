356c8b759677232fe696da3de7e4b532d2a82650
Raj Patil <rajvardhan.patil525@gmail.com>
2025-12-30 14:45:30 +0530

    Update pointers.cpp

--- basics/pointers.cpp
+++ basics/pointers.cpp
@@ -1,1 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int x = 10;
+    int* ptr = &x;
+
+    cout << "Value of x: " << x << endl;
+    cout << "Address of x: " << &x << endl;
+    cout << "Value using pointer: " << *ptr << endl;
+
+    int* arr = new int[5];
+    for (int i = 0; i < 5; i++) {
+        arr[i] = i * 10;
+    }
+
+    for (int i = 0; i < 5; i++) {
+        cout << arr[i] << " ";
+    }
+
+    delete[] arr;
+    return 0;
+}
 
