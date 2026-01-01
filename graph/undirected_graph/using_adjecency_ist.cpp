0d9007e1f64697910e5f2452a11c7ec887c67080
Raj Patil <rajvardhan.patil525@gmail.com>
2026-01-01 19:47:26 +0530

    Create using_adjecency_ist.cpp

---
+++ graph/undirected_graph/using_adjecency_ist.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Graph {
+    int V;
+    vector<vector<int>> adj;
+
+public:
+    Graph(int V) {
+        this->V = V;
+        adj.resize(V);
+    }
+
+    void addEdge(int u, int v) {
+        adj[u].push_back(v);
+        adj[v].push_back(u);
+    }
+
+    void printGraph() {
+        for (int i = 0; i < V; i++) {
+            cout << i << " -> ";
+            for (int v : adj[i])
+                cout << v << " ";
+            cout << endl;
+        }
+    }
+};
+
+int main() {
+    Graph g(5);
+
+    g.addEdge(0, 1);
+    g.addEdge(0, 4);
+    g.addEdge(1, 2);
+    g.addEdge(1, 3);
+    g.addEdge(1, 4);
+    g.addEdge(2, 3);
+    g.addEdge(3, 4);
+
+    g.printGraph();
+    return 0;
+}
